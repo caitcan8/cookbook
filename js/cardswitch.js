24b2a1b63e90a8c0f519651e8da0af3fd9d051f8
caitcan8 <159196277+caitcan8@users.noreply.github.com>
2025-04-19 22:27:45 -0400

    Add files via upload

--- js/cardswitch.js
+++ js/cardswitch.js
@@ -10,74 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
     
 
     cards.forEach(meal => {
-       
-      // Add event listeners to each card within the set
+     
       meal.querySelectorAll('div').forEach(card => {
         
-        // card.addEventListener('focus', () => {
-        //     if(!ended){
-        //         ended = true;
-        //         return;
-        //     }
-        //     // ++count;
-        //     // if(count > 2){
-        //     //     count = 0;
-        //     //     return;
-        //     // }
-        //     if (tabPressed && count > 1) {
-        //         count = 0;
-        //         return;
-        //     }
-        //     //when the card is clicked
-        //   if (card.classList.contains('activepage')) {
-        //     return; //if its already active dont do anything
-        //   }
-  
-        //   //get current classes 
-        //   const a = meal.querySelector('.activepage');
-        //   const b = meal.querySelector('.secondpage');
-        //   const c = meal.querySelector('.lastpage');
-  
-        //   //swap the inner html of the cards correctly
-        //   if (card.classList.contains('secondpage')) {
-        //     //if user selects second page
-        //     const aContent = a.innerHTML;
-        //     const bContent = b.innerHTML;
-        //     const cContent = c.innerHTML;
-  
-        //     //switch inner html
-        //     a.innerHTML = bContent;
-        //     b.innerHTML = cContent;
-        //     c.innerHTML = aContent;
-  
-        //   } else if (card.classList.contains('lastpage')) {
-        //     // if user selects last page
-        //     const aContent = a.innerHTML;
-        //     const cContent = c.innerHTML;
-  
-        //     // Swap the content
-        //     a.innerHTML = cContent;
-        //     c.innerHTML = aContent;
-        //   }
-        //   //change the focus to A 
-        //   ended = false;
-        //   //a.focus();
-
-
-        //   if (tabPressed) {
-        //     count++;
-        //     tabPressed = false; // Reset the Tab key flag after the action
-        // }
-        // else{
-        //     count = 0;
-        // }
-        // if (!(tabPressed && count >= 2)) {
-        //     a.focus();
-        // }
-          
-          
-        // });
-
         //let ended = true;
         document.addEventListener('keydown', (e) => {
             if (e.key === 'Tab') {
